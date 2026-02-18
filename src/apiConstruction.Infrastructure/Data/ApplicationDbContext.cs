51849e772066bb8b1de9c548756b50e9722d7d78
Germán Mora <dmm71317@gmail.com>
2026-02-17 19:16:42 -0600

    WIP: Se continua construyendo el Contexto DB en Infrastructure

--- src/apiConstruction.Infrastructure/Data/ApplicationDbContext.cs
+++ src/apiConstruction.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using apiConstruction.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace apiConstruction.Infrastructure.Data;
 
@@ -10,14 +11,30 @@ public class ApplicationDbContext : DbContext
     {
     }
 
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Department> Departments => Set<Department>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        // Aplicar configuraciones
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        // Aplicar configuraciones de entidades
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        //Seed data inicial (opcional - datos de prueba)
+        SeedData(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        //Actualizar automáticamente FechaActualizacion
+        var entries = ChangeTracker
+        .Entries()
+        .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        foreach (var entry in entries)
+        {
+            
+        }
     }
 }
